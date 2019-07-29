1def9722c0ebaa23eb2b6a2d96eda93bc806b7b5
ji5485 <ji5485@naver.com>
2019-07-30 00:39:04 +0900

    Modify settings

--- likelion_blog/settings.py
+++ likelion_blog/settings.py
@@ -123,5 +123,6 @@ STATIC_URL = '/static/'
 
 # settings.py
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url db_from_env = dj_database_url.config(conn_max_age=500)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
