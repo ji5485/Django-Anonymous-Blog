e5339df4ff9ee2a3e83e240e3d2f30bb15e5e922
ji5485 <ji5485@naver.com>
2019-07-30 16:33:16 +0900

    Modify for Heroku

--- likelion_blog/settings.py
+++ likelion_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '(p7p0#$^HHFGHHdggtherververvew
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- likelion_blog/wsgi.py
+++ likelion_blog/wsgi.py
@@ -10,10 +10,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'likelion_blog.settings')
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
 
 from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
